80a45fccce901825c761739db383942281bfb8e3
Shelby Hanna <144790272+Shanna36@users.noreply.github.com>
2023-10-18 20:33:46 +0100

    acting like a vehicle
    Player is acting like a vehicle.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,27 +7,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
+    public float rotationSpeed = 500.0f;
     private Rigidbody playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerRb.freezeRotation = true;  // Ensure physics doesn't affect rotation
     }
 
     // Update is called once per frame
-    void Update()
+   void Update()
+{
+    float horizontalInput = Input.GetAxis("Horizontal");
+    float verticalInput = Input.GetAxis("Vertical");
+
+    float rotation = horizontalInput * rotationSpeed * 3 * Time.deltaTime;
+    transform.Rotate(Vector3.up, rotation);
+
+    // Forward movement
+    if (verticalInput > 0)
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        horizontalInput *= -1f;
-
-        // Combine the input values into a single vector
-        Vector3 movement = new Vector3(verticalInput, 0, horizontalInput);
-
-        // Apply the movement to the player's position
-        transform.Translate(movement * Time.deltaTime * speed);
-
+        Vector3 forwardMovement = transform.forward * verticalInput * speed;
+        transform.Translate(forwardMovement * Time.deltaTime, Space.World);
     }
+    // Backward movement
+    else if (verticalInput < 0)
+    {
+        Vector3 backwardMovement = -transform.forward * -verticalInput * speed;
+        transform.Translate(backwardMovement * Time.deltaTime, Space.World);
+    }
+}
+
 }
