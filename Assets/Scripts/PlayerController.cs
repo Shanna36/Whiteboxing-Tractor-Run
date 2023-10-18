1e41e3080bc068a507cd1c1a45a8846b0264aa11
Shelby Hanna <144790272+Shanna36@users.noreply.github.com>
2023-10-18 21:16:59 +0100

    tidy code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,4 @@
-
-using System.Collections.Specialized;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,6 +6,10 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float rotationSpeed = 500.0f;
     private Rigidbody playerRb;
+
+    public float zUpperBound = 140.0f;
+    public float zLowerBound = -154.0f;
+    public float xBound = 100.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,26 +19,53 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-   void Update()
-{
-    float horizontalInput = Input.GetAxis("Horizontal");
-    float verticalInput = Input.GetAxis("Vertical");
-
-    float rotation = horizontalInput * rotationSpeed * 3 * Time.deltaTime;
-    transform.Rotate(Vector3.up, rotation);
-
-    // Forward movement
-    if (verticalInput > 0)
+    void Update()
     {
-        Vector3 forwardMovement = transform.forward * verticalInput * speed;
-        transform.Translate(forwardMovement * Time.deltaTime, Space.World);
+        MovePlayer();
     }
-    // Backward movement
-    else if (verticalInput < 0)
-    {
-        Vector3 backwardMovement = -transform.forward * -verticalInput * speed;
-        transform.Translate(backwardMovement * Time.deltaTime, Space.World);
-    }
-}
 
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        float rotation = horizontalInput * rotationSpeed * 3 * Time.deltaTime;
+        transform.Rotate(Vector3.up, rotation);
+
+        // Forward movement
+        if (verticalInput > 0)
+        {
+            Vector3 forwardMovement = transform.forward * verticalInput * speed;
+            transform.Translate(forwardMovement * Time.deltaTime, Space.World);
+        }
+        // Backward movement
+        else if (verticalInput < 0)
+        {
+            Vector3 backwardMovement = -transform.forward * -verticalInput * speed;
+            transform.Translate(backwardMovement * Time.deltaTime, Space.World);
+        }
+
+        ConstrainPlayerPosition();
+    }
+
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z > zUpperBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zUpperBound);
+        }
+        else if (transform.position.z < zLowerBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zLowerBound);
+        }
+
+        if (transform.position.x > xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -xBound)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+        }
+    }
 }
