ebc11b6abef3a9710a7beeaeb0863163d0adad01
Shelby Hanna <144790272+Shanna36@users.noreply.github.com>
2023-10-26 20:45:46 +0100

    added follow player script to camera

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset = new Vector3(0.0f, 5.0f, 0.5f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+             transform.position = player.position + offset;
+            transform.LookAt(player.position);
+        }
+    }
+}
