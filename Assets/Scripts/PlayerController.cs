10915d313fdf4e704e6d706af9bb884c8878e810
Shelby Hanna <144790272+Shanna36@users.noreply.github.com>
2023-10-27 21:39:37 +0100

    skybox and terrain

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,31 +5,46 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
     public float rotationSpeed = 500.0f;
-    private Rigidbody playerRb;
 
     public float zUpperBound = 140.0f;
     public float zLowerBound = -154.0f;
     public float xBound = 100.0f;
+    
+    private Rigidbody playerRb; 
+    private Terrain terrain;  // Reference to the terrain
+    private TerrainData terrainData;  // Reference to the terrain data
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerRb.freezeRotation = true;  // Ensure physics doesn't affect rotation
+
+        terrain = GameObject.FindWithTag("Terrain").GetComponent<Terrain>();
+        terrainData = terrain.terrainData;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();
+   
     }
+
+    void FixedUpdate()
+{
+    MovePlayer();
+    ConstrainPlayerToTerrain();
+}
 
     void MovePlayer()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        float rotation = horizontalInput * rotationSpeed * 3 * Time.deltaTime;
+        float rotation = horizontalInput * rotationSpeed * 5 * Time.deltaTime;
         transform.Rotate(Vector3.up, rotation);
 
         // Forward movement
@@ -48,6 +63,29 @@ public class PlayerController : MonoBehaviour
         ConstrainPlayerPosition();
     }
 
+ void ConstrainPlayerToTerrain()
+    {
+        float terrainHeight = GetTerrainHeightAtPlayerPosition();
+        Vector3 newPosition = new Vector3(transform.position.x, terrainHeight, transform.position.z);
+        playerRb.MovePosition(newPosition);
+    }
+
+float GetTerrainHeightAtPlayerPosition()
+    {
+        // Get normalized terrain coordinates based on the player's position
+        float terrainX = (transform.position.x - terrain.transform.position.x) / terrainData.size.x;
+        float terrainZ = (transform.position.z - terrain.transform.position.z) / terrainData.size.z;
+
+        // Convert normalized coordinates to terrain coordinates
+        int heightmapX = Mathf.Clamp(Mathf.FloorToInt(terrainX * terrainData.heightmapResolution), 0, terrainData.heightmapResolution - 1);
+        int heightmapZ = Mathf.Clamp(Mathf.FloorToInt(terrainZ * terrainData.heightmapResolution), 0, terrainData.heightmapResolution - 1);
+
+        // Get the height from the terrain's heightmap data
+        float[,] heights = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
+        float terrainHeight = heights[heightmapX, heightmapZ] * terrainData.size.y;
+
+        return terrainHeight;
+    }
     void ConstrainPlayerPosition()
     {
         if (transform.position.z > zUpperBound)
@@ -68,4 +106,5 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
         }
     }
+
 }
