127f40f6d05d9518ecc2829c2815495468be81cd
Shelby Hanna <144790272+Shanna36@users.noreply.github.com>
2023-10-29 20:45:45 +0000

    wheat not wonky and confined to a manageable field
    Renamed some variables to make them more descrptive too. Holding onto to Euler angles for the wheat, though they are commented out just in case i dont like how it interacts with the steeper terrain

--- Assets/Scripts/SpawnWheat.cs
+++ Assets/Scripts/SpawnWheat.cs
@@ -6,33 +6,61 @@ public class SpawnWheat : MonoBehaviour
 {
    public GameObject wheatPrefab; 
 public Terrain terrain;
-public int numberOfWheat = 1000; // Number of wheat instances you want to spawn
+public int numberOfWheat = 200; // Number of wheat instances you want to spawn
 
 void Start()
 {
-    float rowDistance = 3.0f;   // Distance between each row
-    float wheatSpacing = 1.0f;  // Distance between each wheat in a row
+    int numberOfWheat = 300; // Adjust as needed
 
-    int rows = Mathf.FloorToInt((207 + 223) / rowDistance);  // Total rows that fit in the z boundary
-    int columns = Mathf.FloorToInt((470 + 101) / wheatSpacing); // Total columns that fit in the x boundary
+   
+    float fieldStartX = -290;
+    float fieldEndX = -189;
+    float fieldAreaWidth = fieldEndX - fieldStartX;
 
-    for (int rowIndex = 0; rowIndex < rows; rowIndex++)
+    float fieldStartZ = 100;
+    float fieldEndZ = 200;
+    float fieldAreaDepth = fieldEndZ - fieldStartZ;
+
+    // Calculate potential rows and columns with given distances
+    int maxRows = Mathf.FloorToInt(fieldAreaDepth);
+    int maxColumns = Mathf.FloorToInt(fieldAreaWidth);
+
+    // Calculate total number of positions
+    int totalPositions = maxRows * maxColumns;
+
+    // Adjust the spacing if the required number of wheat is less than total positions
+    if (numberOfWheat < totalPositions)
     {
-        for (int columnIndex = 0; columnIndex < columns; columnIndex++)
+        float spacingFactor = Mathf.Sqrt(totalPositions / (float)numberOfWheat);
+        maxRows = Mathf.FloorToInt(maxRows / spacingFactor);
+        maxColumns = Mathf.FloorToInt(maxColumns / spacingFactor);
+    }
+
+    float rowDistance = fieldAreaDepth / maxRows;
+    float wheatSpacing = fieldAreaWidth / maxColumns;
+
+    for (int rowIndex = 0; rowIndex < maxRows; rowIndex++)
+    {
+        for (int columnIndex = 0; columnIndex < maxColumns; columnIndex++)
         {
-            float x = -470 + columnIndex * wheatSpacing;
-            float z = -223 + rowIndex * rowDistance;
+            float x = fieldStartX + columnIndex * wheatSpacing;
+            float z = fieldStartZ + rowIndex * rowDistance;
 
-            // The rest is similar to before, sampling height and spawning the wheat
-            Vector3 localPoint = terrain.transform.InverseTransformPoint(new Vector3(x, 0, z));
-            float y = terrain.SampleHeight(localPoint);
-            y += terrain.transform.position.y + 0.1f;
+            // Get the height of the terrain at this x, z coordinate
+            float y = terrain.SampleHeight(new Vector3(x, 0, z));
 
-            Vector3 normal = terrain.terrainData.GetInterpolatedNormal((x + 470) / (terrain.terrainData.size.x + 369), (z + 223) / (terrain.terrainData.size.z + 430));
-            Quaternion rotation = Quaternion.LookRotation(Vector3.Cross(Vector3.right, normal), normal);
-            
+            y += terrain.transform.position.y;
+
+            Vector3 normal = terrain.terrainData.GetInterpolatedNormal((x - fieldStartX) / (fieldEndX - fieldStartX), (z - fieldStartZ) / (fieldEndZ - fieldStartZ));
+            Quaternion rotation = Quaternion.Euler(10f, 0f, 0f);
+            //rotation *= Quaternion.Euler(70f, 0f, 0f); //fixing my model's wonky rotation
+
+
             Instantiate(wheatPrefab, new Vector3(x, y, z), rotation);
         }
     }
 }
+
+
+
 }
